904b29a40d1eae4a3ee8b3c5db489d134a3827de
Pave1Pal <pd271828@gmail.com>
2024-02-29 11:38:53 +0300

    chore(frontend): vite config cros origin isolated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 import topLevelAwait from "vite-plugin-top-level-await";
-import svgr from 'vite-plugin-svgr';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import svgr from "vite-plugin-svgr";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    TanStackRouterVite({ routeFileIgnorePrefix: '-' }),
-    checker({ typescript: true, overlay: { initialIsOpen: false, position: 'br' } }),
+    TanStackRouterVite({ routeFileIgnorePrefix: "-" }),
+    checker({ typescript: true, overlay: { initialIsOpen: false, position: "br" } }),
     topLevelAwait(),
     svgr(),
   ],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    }
+  }
 });
