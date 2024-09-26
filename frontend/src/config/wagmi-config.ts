7a58ffb89833e10bece1d76d5c861d7571abffc0
Biryukov Ilya <bis21080000@gmail.com>
2024-09-26 17:05:08 +0300

    chore(frontend): allow wallet connection to any chain

--- frontend/src/config/wagmi-config.ts
+++ frontend/src/config/wagmi-config.ts
@@ -1,7 +1,9 @@
 import { getConnectorClient as _getConnectorClient } from '@wagmi/core';
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
-import { mainnet } from 'wagmi/chains';
+import * as chains from 'wagmi/chains';
+import { config } from '@/config/index.ts';
+
 
 const projectId = '210e2cdbd47e9ccfd099225022759a11';
 const metadata = {
@@ -11,9 +13,17 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
+const allChains = Object.values(chains).filter((chain: unknown): chain is chains.Chain => {
+  const isChain = typeof chain === 'object' && !!chain
+    && 'id' in chain && 'name' in chain && 'nativeCurrency' in chain;
+  if (!isChain) return false;
+  const isTestnet = 'testnet' in chain && chain.testnet;
+  return config.isDev || !isTestnet;
+}) as unknown as [chains.Chain, ...chains.Chain[]];
+
 export const wagmiConfig = defaultWagmiConfig({
   projectId, metadata,
-  chains: [mainnet],
+  chains: allChains,
   auth: {
     socials: [],
     email: false,
@@ -25,6 +35,7 @@ export const web3modal = createWeb3Modal({
   projectId, metadata,
   wagmiConfig,
   themeVariables: { '--w3m-border-radius-master': '2px' },
+  allowUnsupportedChain: true,
 });
 
 export const getConnectorClient = _getConnectorClient.bind(undefined, wagmiConfig);
