f5fdb1f929aa7ff152013a8928018a3da244aeab
Biryukov Ilya <bis21080000@gmail.com>
2024-10-07 04:22:35 +0300

    chore(frontend): fix build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { WagmiProvider } from 'wagmi';
+import { appName } from '@/config/constants.ts';
 import { queryClient } from './config/query-client.ts';
 import { wagmiConfig } from './config/wagmi-config.ts';
 import { routeTree } from './routeTree.gen.ts';
